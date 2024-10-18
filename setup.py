16f29185dcc75f7086b0aa6d35139a5d6ca1e80c
Sharath N <sharath2523@gmail.com>
2024-10-18 23:17:05 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical-Bot',
+    version= '0.0.0',
+    author= 'Sharath N',
+    author_email= 'sharath2523@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
